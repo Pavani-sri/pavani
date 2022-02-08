1d3fbe5bb17ca7cefdd2a7aa8c2acfc2cffacd33
Pavani-sri <padarthipavanisri@gmail.com>
2022-02-08 10:21:05 +0000

    PUSH

--- sampleCucumber/target/cucumber-reports/cucumber-pretty/report.js
+++ sampleCucumber/target/cucumber-reports/cucumber-pretty/report.js
@@ -43,34 +43,195 @@ formatter.match({
   "location": "DragDrop.open_LambdaTests_Selenium_Playground()"
 });
 formatter.result({
-  "duration": 2957369318,
+  "duration": 1711343767,
   "status": "passed"
 });
 formatter.match({
   "location": "DragDrop.click_Drag_Drop_Sliders_under_Progress_Bars_Sliders()"
 });
 formatter.result({
-  "duration": 1642168776,
+  "duration": 1324679438,
   "status": "passed"
 });
 formatter.match({
   "location": "DragDrop.select_the_slider_Default_value()"
 });
 formatter.result({
-  "duration": 28552,
+  "duration": 33692,
   "status": "passed"
 });
 formatter.match({
   "location": "DragDrop.drag_the_bar_to_make_it_by()"
 });
 formatter.result({
-  "duration": 4190266825,
+  "duration": 4372261583,
   "status": "passed"
 });
 formatter.match({
   "location": "DragDrop.validating_whether_the_range_value_shows()"
 });
 formatter.result({
-  "duration": 656756948,
+  "duration": 1405680604,
   "status": "passed"
+});
+formatter.uri("InputFormSubmit.feature");
+formatter.feature({
+  "line": 1,
+  "name": "InputFormSubmit",
+  "description": "",
+  "id": "inputformsubmit",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 2,
+  "name": "Form filling scenario",
+  "description": "",
+  "id": "inputformsubmit;form-filling-scenario",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 3,
+  "name": "Open the lambdatest selenium-playground page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "click Input Form Submit under Input Forms",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 5,
+  "name": "Click Submit without filling in any information in the form",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "Assert \"Please fill out this field.\" error message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "Fill in Name, Email, and other fields",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "click Submit",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "Once submitted, validate the success message \"Thanks for contacting us, we will get back to you shortly.\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "InputFormSubmit.open_the_lambdatest_selenium_playground_page()"
+});
+formatter.result({
+  "duration": 1425437350,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InputFormSubmit.click_Input_Form_Submit_under_Input_Forms()"
+});
+formatter.result({
+  "duration": 842991393,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InputFormSubmit.click_Submit_without_filling_in_any_information_in_the_form()"
+});
+formatter.result({
+  "duration": 458571826,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Please fill out this field.",
+      "offset": 8
+    }
+  ],
+  "location": "InputFormSubmit.assert_error_message(String)"
+});
+formatter.result({
+  "duration": 444620449,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InputFormSubmit.fill_in_Name_Email_and_other_fields()"
+});
+formatter.result({
+  "duration": 4133369160,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InputFormSubmit.click_Submit()"
+});
+formatter.result({
+  "duration": 467984291,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Thanks for contacting us, we will get back to you shortly.",
+      "offset": 46
+    }
+  ],
+  "location": "InputFormSubmit.once_submitted_validate_the_success_message(String)"
+});
+formatter.result({
+  "duration": 1440242761,
+  "status": "passed"
+});
+formatter.uri("SimpleFormDemo.feature");
+formatter.feature({
+  "line": 2,
+  "name": "SimpleDemoForm",
+  "description": "",
+  "id": "simpledemoform",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "verification of entered message",
+  "description": "",
+  "id": "simpledemoform;verification-of-entered-message",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "Open LambdaTests Selenium Playground from",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Click Simple Form Demo under Input Forms",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "Validate that the URL contains simple-form-demo",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "Create a variable to enter values in the Enter Message text box",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "Click Get Checked Value",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "Validate whether the same text message is displayed under the Your Message section",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SimpleFormDemo.open_LambdaTests_Selenium_Playground_from()"
 });
